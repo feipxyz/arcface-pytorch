c1d18d8f50da031cf5b9cbb4fe8aa8e986d4be49
feipxyz <feipxyz@163.com>
2022-04-04 03:47:25 +0000

    add run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 # 准备数据
-
 shuf data/cleaned_list.txt > data/all.list
 head data/all.list -n 400000 > data/train.txt
 tail data/all.list -n 55594 > data/val.txt
+
+cp /content/drive/MyDrive/dataset/lfw-align-128.tar.gz data
+tar -xvf data/lfw-align-128.tar.gz
