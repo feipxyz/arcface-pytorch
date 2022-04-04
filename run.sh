bb3c97edcde2b675714de2fa40f6faf8e017f46e
feipxyz <feipxyz@163.com>
2022-04-04 03:58:04 +0000

    update run

--- run.sh
+++ run.sh
@@ -1,7 +1,17 @@
 # 准备数据
-shuf data/cleaned_list.txt > data/all.list
-head data/all.list -n 400000 > data/train.txt
-tail data/all.list -n 55594 > data/val.txt
+cd data
+cp /content/drive/MyDrive/dataset/CASIA-WebFace-clean-up.zip .
+unzip CASIA-WebFace-clean-up.zip 
 
-cp /content/drive/MyDrive/dataset/lfw-align-128.tar.gz data
-tar -xvf data/lfw-align-128.tar.gz
+shuf cleaned_list.txt > all.list
+head all.list -n 400000 > train.txt
+tail all.list -n 55594 > val.txt
+
+cp /content/drive/MyDrive/dataset/lfw-align-128.tar.gz .
+tar -xvf lfw-align-128.tar.gz
+
+cd -
+
+# 准备预训练模型
+mkdir checkpoints
+cp /content/drive/MyDrive/model_facerecogniztion/resnet18_110.pth checkpoints/
